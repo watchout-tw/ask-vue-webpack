70126662579ca296299c50b28ae90d723d36b826
chihao <chihaoyo@gmail.com>
2017-07-05 15:23:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="app">
+  <NavigationWithIdentity :channel="channel" :isAuthenticated.sync="isAuthenticated" :modalAuthIsShown.sync="modalAuthIsShown"></NavigationWithIdentity>
+  <ModalAuth v-if="modalAuthIsShown" :modalAuthIsShown.sync="modalAuthIsShown" :isAuthenticated.sync="isAuthenticated"></ModalAuth>
+  <FooterStandard></FooterStandard>
+</div>
+</template>
+
+<script>
+import dataStore from 'common/src/lib/dataStore'
+import NavigationWithIdentity from 'common/src/components/Navigation/Identity'
+import ModalAuth from 'common/src/components/Modal/Auth'
+import FooterStandard from 'common/src/components/Footer/Standard'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      channel: dataStore.channels.ask,
+      isAuthenticated: false,
+      modalAuthIsShown: false
+    }
+  },
+  components: {
+    NavigationWithIdentity,
+    ModalAuth,
+    FooterStandard
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~common/src/styles/resources';
+@import '~common/src/styles/main';
+</style>
